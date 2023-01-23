1575efaaad1f56a8cd07e35fee34d7ee134ff2c0
Jason Forsyth <forsy2jb@jmu.edu>
2023-01-23 17:43:51 -0500

    Add module1 assignments

---
+++ Module 1/4 - Assignments/elon_money.py
@@ -0,0 +1,21 @@
+"""
+This problem requires you to calculate compounding interest and final value of a  US treasury deposit based upon
+current interest rates (that will be provided). Your analysis should return the final value of the investment
+after a 1-year and 20-year period. The final values should be stored in the variables "ten_year_final"
+and "twenty_year_final", respectively. Perform all your calculations in this file. Do not perform the calculations by hand
+and simply write in the final result.
+
+Prompt: On October 27th, 2022, Elon Musk purchased Twitter for $44B in total, with reportedly $33B of his own money. Since
+that time, it appears this investment has not worked out. If Elon has instead bought $44B of US Treasury Bonds, how much
+would his investment be worth in 10-year and 20-year bonds? Assume the 10-year bonds pay 3.96%,
+the 20-year bonds pay 4.32%, with each compounding annually.
+"""
+
+### all your code below ###
+
+
+# final answer for 10-year
+ten_year_final = None
+
+# final answer for 20-year
+twenty_year_final = None

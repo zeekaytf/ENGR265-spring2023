b729e9faade5445f40a02562ccc644ca545af0e7
zkreitzer <123010106+zeekaytf@users.noreply.github.com>
2023-01-31 10:44:56 -0500

    Update 1.2.0 - Operators_Tutorial.py

--- Module 1/2 - Tutorials/1.2.0 - Operators_Tutorial.py
+++ Module 1/2 - Tutorials/1.2.0 - Operators_Tutorial.py
@@ -9,19 +9,20 @@ vector_one = 16
 vector_two = 23
 
 # First, let's square each vector. Use whichever method you like:
-one_squared = -1
-two_squared = -1
+one_squared = vector_one**2
+two_squared = vector_two**2
 
 # Next, let's add the two vectors together:
-vector_sum = -1
+vector_sum = (one_squared + two_squared)
 
 # Finally, find the square root of the sum. Use any method:
-magnitude = -1
+magnitude = vector_sum**(1/2)
 
 # Use a print statement to check your work!
 # Answer should be around 28.
-print()
+print(magnitude)
 
 # Now, try to do that process all in one step!
-magnitude_two = -1
-print()
+magnitude_two = (vector_one**2 + vector_two**2)**(1/2)
+print(magnitude_two)
+

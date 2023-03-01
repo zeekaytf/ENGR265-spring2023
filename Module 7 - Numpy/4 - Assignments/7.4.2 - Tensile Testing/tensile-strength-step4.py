a28e9e72b117e1907ed28ad0f03eba970e16e0e5
Jason Forsyth <forsy2jb@jmu.edu>
2023-02-28 20:57:24 -0500

    Add offset yield assignment

--- Module 7 - Numpy/4 - Assignments/7.4.2 - Tensile Testing/tensile-strength-step4.py
+++ Module 7 - Numpy/4 - Assignments/7.4.2 - Tensile Testing/tensile-strength-step4.py
@@ -60,13 +60,9 @@ def calculate_stress(force, sample_diameter):
     :return: An array of stresses experienced by the sample in Kilo Pascals (KPa)
     """
 
-    # calculate the cross-section area (mm^2)
-    ### your code here ###
+    ### YOUR SOLUTION FROM STEP 1 TEMPLATE HERE ###
 
-    # delete this line and replace it with your own
-    stress = None
-
-    return stress
+    return None
 
 
 def calculate_max_strength_strain(strain, stress):
@@ -79,25 +75,20 @@ def calculate_max_strength_strain(strain, stress):
     Fracture Strain: the maximum strain experienced before fracture
     """
 
-    # calculate the maximum stress experienced
-    ultimate_tensile_stress = -1
+    ### YOUR SOLUTION FROM STEP 2 TEMPLATE HERE ###
 
-    # calculate the maximum strain experienced
-    fracture_strain = -1
-
-    return ultimate_tensile_stress, fracture_strain
-
+    return -1, -1
 
 def calculate_elastic_modulus(strain, stress):
     """
-    Given a set of stress strain data, use the Secant Modulus at 40% method to determine
-    the elastic modulus
-    :param strain: An array of Strain data (MPa)
-    :param stress: An array of Strain data
-    :return:
-    linear_index: the index within the strain/stress data that is the end of the linear region
-    slope: the slope for the linear region of the strain/stress data
-    intercept: y-intercept for linear region best fit of strain/stress data
+        Given a set of stress strain data, use the Secant Modulus at 40% method to determine
+        the elastic modulus
+        :param strain:
+        :param stress:
+        :return:
+        linear_index: the index within the strain/stress data that is the end of the linear region
+        slope: the slope for the linear region of the strain/stress data
+        intercept: y-intercept for linear region best fit of strain/stress data
     """
 
     # dummy variables the function should over write
@@ -105,36 +96,7 @@ def calculate_elastic_modulus(strain, stress):
     slope = None
     intercept = None
 
-    # Step 3a: find the point that is 40% of peak stress
-    # use from 0 to that value to create a linear plot
-
-    ### your code below ###
-    secant_strain = -1
-
-    # Step 3b: find the intersection between 40% line and the curvey
-    # take the abs() difference between the stress vector and secant_straint point
-
-    ### your code below ###
-    diffs = -1
-
-    # use np.argmin() to find the minimum of the diffs array.
-    # this will be the INDEX of the point in stress-strain that is closest to
-    # secant_strain intersection
-
-    # uncomment the line below and replace with your own
-    # linear_index = ....
-
-    # Step 3c: down select to linear region for stress and strain
-    # using list slicing. Uncomment lines below
-    # linear_stress = stress[# list slice#]
-    # linear_strain = strain[#list slice#]
-
-    # Step 3d: find least squares fit to a line in the linear region
-    # use 1-degree polynominal fit (line) from np.polyfit
-    # save the slope and intercept so we can plot the line later
-
-    # uncomment the line below and call np.polyfit
-    # slope, intercept = ....
+    ### YOUR SOLUTION FROM STEP 3 TEMPLATE HERE ###
 
     return linear_index, slope, intercept
 
@@ -164,7 +126,6 @@ def calculate_percent_offset(slope, strain, stress):
 
     return offset_line, intercept_index
 
-
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
 
@@ -187,12 +148,6 @@ if __name__ == "__main__":
     # sample diameter (mm), time (s), displacement (mm), force (kN), and strain (%)
     sample_diameter, time, displacement, force, strain = parse_tensile_file(path_to_file)
 
-    #plt.scatter(strain,force,label="Force - Strain")
-    #plt.xlabel("Strain (%)")
-    #plt.ylabel("Force (kN)")
-    #plt.title("Force Applied and Resulting Strain")
-    #plt.show()
-
     # Step #1: Given the forces and sample diameter, calculate the strain
     stress = calculate_stress(force, sample_diameter)
 
@@ -202,7 +157,7 @@ if __name__ == "__main__":
 
     # use scatter plot so we don't assume a line (yet)
     plt.scatter(strain, stress, label="Stress - Strain")
-    plt.xlabel('Strain (%)')
+    plt.xlabel('Strain (mm/mm)')
     plt.ylabel('Stress (MPa)')
     plt.title('Stress-Strain Curve for Sample ' + sample_name)
     plt.show()
@@ -224,7 +179,7 @@ if __name__ == "__main__":
     # to determine elastic modulus
 
     linear_index, slope, intercept = calculate_elastic_modulus(strain, stress)
-    
+
     if linear_index is None or slope is None or intercept is None:
         print("Incorrect value determined by calculate_elastic_modulus() did you implement that function?")
         sys.exit(-1)
@@ -233,7 +188,7 @@ if __name__ == "__main__":
 
     # show the original curve indicating the secant modulus at 40%
     plt.scatter(strain, stress, label="Stress - Strain")
-    plt.xlabel('Strain (%)')
+    plt.xlabel('Strain (mm/mm)')
     plt.ylabel('Stress (MPa)')
     plt.title('Stress-Strain Curve for Sample ' + sample_name)
 
@@ -247,14 +202,21 @@ if __name__ == "__main__":
     linear_stress = stress[0:linear_index]
 
     plt.scatter(linear_strain, linear_stress, label="Stress - Strain")
-    plt.xlabel('Strain (%)')
+    plt.xlabel('Strain (mm/mm)')
     plt.ylabel('Stress (MPa)')
     plt.title('Linear Region for Sample ' + sample_name + ' with best fit')
+
+    # compute line y=mx+b
+    best_fit_line = slope * linear_strain + intercept
+    plt.plot(linear_strain, best_fit_line, label="Best Linear Fit")
+
+    plt.legend()
+    plt.show()
 
     ### Step 4: calculate 0.2% yield strength ###
     offset_line, intercept_index = calculate_percent_offset(slope, strain, stress)
 
-    if offset_line is None or intercept_index == -1:
+    if offset_line is None or intercept_index==-1:
         print("Calculate Percent Offset Returned None or -1. Did you return the correct values?")
         sys.exit(-1)
 
